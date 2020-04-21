c0bdc9a8768346d3d37aa4fdc52d903aa51bfbb1
JonathanPucci <jonathanpucci06@gmail.com>
2020-04-21 22:25:25 +0200

    testurl

--- eventLink/eventLink.js
+++ eventLink/eventLink.js
@@ -1,6 +1,6 @@
 function getMobileOperatingSystem() {
     var userAgent = navigator.userAgent || navigator.vendor || window.opera;
-
+    console.log(userAgent)
     // Windows Phone must come first because its UA also contains "Android"
     if (/windows phone/i.test(userAgent)) {
         return "Windows Phone";
