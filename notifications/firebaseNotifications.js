365c679e6064710e25078fbecbf0c189f7063c1d
JonathanPucci <jonathanpucci06@gmail.com>
2020-04-29 01:31:12 +0200

    added firebase key

--- notifications/firebaseNotifications.js
+++ notifications/firebaseNotifications.js
@@ -11,14 +11,19 @@ var firebaseConfig = {
     appId: "1:87122479659:web:9ab68df8f297560e24f86e",
     measurementId: "G-02QM7Q1GGL"
 };
+
+var serviceAccount = require("../sportcoapp-firebase-adminsdk-jchxw-641566c3e5.json");
+
 admin.initializeApp({
-    credential: admin.credential.applicationDefault(),
-    databaseURL: 'https://sportcoapp.firebaseio.com'
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://sportcoapp.firebaseio.com"
 });
+// Initialize Firebase
+// admin.initializeApp(firebaseConfig);
 // admin.analytics();
 
 
- sendNotifToUserWithToken = async (notif, user, token)=>{
+sendNotifToUserWithToken = async (notif, user, token) => {
 
     var notification =
     {
@@ -32,7 +37,7 @@ admin.initializeApp({
         date: JSON.stringify((new Date()).toJSON()),
         data_type: notif.data_type.toString(),
         data_value: notif.data_value.toString(),
-        data_value2: notif.data_value2 != undefined? notif.data_value2.toString() : '',
+        data_value2: notif.data_value2 != undefined ? notif.data_value2.toString() : '',
         message_type_app_key: notif.message_type.toString()
     }
     var messageData = {
@@ -58,6 +63,16 @@ admin.initializeApp({
         console.log('Error sending message:', error);
     };
 }
+
+// let user = { user_id: 1 };
+// let notif = {
+//     message_type: "PARTICIPANT_JOINED",
+//     data_type: "event_id",
+//     data_value: 1,
+//     data_value2: 'https://graph.facebook.com/2934553376621421/picture'
+// }
+// let token = 'ew_GGUma6p8:APA91bGMPVgR0kG7mkr7WQwui1d28_4mdQst4syBJAdnhIyXgFeELaioku-9ufIx8fkHWaxDbWh0S7w5aOqHPNBeh1Jl_OeWNsMdJvtGfMYd_Vmh_FCQwLMBhbV_o6VEjJPfzG8E6yQw';
+// sendNotifToUserWithToken(notif, user, token);
 
 module.exports = {
     sendNotifToUserWithToken: sendNotifToUserWithToken
