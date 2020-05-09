9043df8497f5e588e3126c3c757dc230a90840db
JonathanPucci <jonathanpucci06@gmail.com>
2020-05-09 12:21:00 +0200

    creds in env var

--- notifications/firebaseNotifications.js
+++ notifications/firebaseNotifications.js
@@ -16,7 +16,7 @@ var serviceAccount = {
   "type": "service_account",
   "project_id": "sportcoapp",
   "private_key_id": process.env.FB_PRIVATE_KEY_ID,
-  "private_key": "-----BEGIN PRIVATE KEY-----\n"+process.env.FB_PRIVATE_KEY+"\n-----END PRIVATE KEY-----\n",
+  "private_key": process.env.FB_PRIVATE_KEY.replace(/\\n/g, '\n'),
   "client_email": "firebase-adminsdk-jchxw@sportcoapp.iam.gserviceaccount.com",
   "client_id": "116527214378414383187",
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
