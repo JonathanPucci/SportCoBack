4b65f45cf96dabb70b8dc03a0d6adec4d0bfa6bf
JonathanPucci <jonathanpucci06@gmail.com>
2022-02-12 18:01:23 +0100

    db connection broken

--- dbconnection.js
+++ dbconnection.js
@@ -20,7 +20,9 @@ const cn = {
 // const config =
 //   process.env.DATABASE_URL != undefined ? process.env.DATABASE_URL : cn;
 
-const cnObj = new ConnectionString("postgres://sifsrwtukzmsjn:ef0bad6eff5d7481a7ebde324bd2743bb1c41270368d0ac89b85c69deccfe207@ec2-176-34-97-213.eu-west-1.compute.amazonaws.com:5432/d93n3ksu2f8gha");
+const cnObj0 = new ConnectionString("postgres://sifsrwtukzmsjn:ef0bad6eff5d7481a7ebde324bd2743bb1c41270368d0ac89b85c69deccfe207@ec2-176-34-97-213.eu-west-1.compute.amazonaws.com:5432/d93n3ksu2f8gha");
+const cnObj = new ConnectionString("postgres://wcwvgqweazxzdz:59ee9910036f0cec8fd4fb049f58de2a276c6ed9d7fbbe152c6e4c15f136a871@ec2-54-220-166-184.eu-west-1.compute.amazonaws.com:5432/da2fqr8e7smd6g");
+
 const config = {
   host: cnObj.hostname,
   port: cnObj.port,
